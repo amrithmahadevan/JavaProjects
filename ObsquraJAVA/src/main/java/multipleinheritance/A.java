cd4474f55f47bafe3d956dc315d8f93a4f109b35
AMRITH MAHADEVAN <AMRITH MAHADEVAN@DESKTOP-NK58V81>
2024-02-23 00:15:49 +0530

    Multiple Inheritance

---
+++ ObsquraJAVA/src/main/java/multipleinheritance/A.java
@@ -0,0 +1,7 @@
+package multipleinheritance;
+
+public class A {
+	public void display1() {
+		System.out.println("Hello printed from class A");
+	}
+}
---
+++ ObsquraJAVA/src/main/java/multipleinheritance/B.java
@@ -0,0 +1,8 @@
+package multipleinheritance;
+
+public class B extends A{
+	public void display2() {
+		System.out.println("Hello printed from class B");
+	}
+
+}
