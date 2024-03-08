d32142697ad42e4bf459f0eaf66e4cfc7474ec44
Amrith Mahadevan <160821963+amrithmahadevan@users.noreply.github.com>
2024-03-08 09:49:59 +0530

    Delete ObsquraJAVA/src/main/java/multipleinheritance directory

--- ObsquraJAVA/src/main/java/multipleinheritance/A.java
+++
@@ -1,7 +0,0 @@
-package multipleinheritance;
-
-public class A {
-	public void display1() {
-		System.out.println("Hello printed from class A");
-	}
-}
--- ObsquraJAVA/src/main/java/multipleinheritance/B.java
+++
@@ -1,8 +0,0 @@
-package multipleinheritance;
-
-public class B extends A{
-	public void display2() {
-		System.out.println("Hello printed from class B");
-	}
-
-}
